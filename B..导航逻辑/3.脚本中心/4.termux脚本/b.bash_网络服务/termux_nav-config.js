8a546035e1161ea4027500b07c61517d014a6db1
qq1660396635 <@qq.com>
2025-12-26 18:03:10 +0800

    first commit

--- B..导航逻辑/3.脚本中心/4.termux脚本/b.bash_网络服务/termux_nav-config.js
+++ B..导航逻辑/3.脚本中心/4.termux脚本/b.bash_网络服务/termux_nav-config.js
@@ -4,5 +4,6 @@ window.navConfig = {
     { name: '判断视频是否失效', file: '2.判断视频是否失效.html', icon: '📺', description: '判断视频链接是否失效' },
     { name: 'tm-VNC', file: '3.tm-VNC.html', icon: '🖥️', description: '服务器开机管理' },
     { name: 'tm-uniapp开发', file: '4.tm-uniapp开发.html', icon: '📱', description: 'UniApp开发工具' },
+    { name: 'GITHUB项目推送', file: '5.GITHUB项目推送.html', icon: '🐙', description: 'GitHub项目推送工具' }
   ]
 };
